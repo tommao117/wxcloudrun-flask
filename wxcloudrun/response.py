7f80833d3a456f4414ce0eb5c5b4e7c230cd997f
tommao <tommao@tencent.com>
2024-12-08 21:26:53 +0800

    misc

--- wxcloudrun/response.py
+++ wxcloudrun/response.py
@@ -4,15 +4,14 @@ from flask import Response
 
 
 def make_succ_empty_response():
-    data = json.dumps({'code': 0, 'data': {}})
+    data = json.dumps({})
     return Response(data, mimetype='application/json')
 
 
 def make_succ_response(data):
-    data = json.dumps({'code': 0, 'data': data})
+    data = json.dumps(data)
     return Response(data, mimetype='application/json')
 
 
 def make_err_response(err_msg):
-    data = json.dumps({'code': -1, 'errorMsg': err_msg})
-    return Response(data, mimetype='application/json')
+    return Response(err_msg, mimetype='application/json')
